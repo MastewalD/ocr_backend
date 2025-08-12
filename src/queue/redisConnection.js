bac1f741804845ea31b1444bf729240f553f51d0
Mastewal Degu <126682436+MastewalD@users.noreply.github.com>
2025-08-12 07:34:17 +0300

    Update redisConnection.js

--- src/queue/redisConnection.js
+++ src/queue/redisConnection.js
@@ -1,5 +1,5 @@
 const { Queue } = require('bullmq');
-const connection = { host: '127.0.0.1', port: 6379 }; // your redis config
+const connection = { host: '127.0.0.1', port: 6379 }; 
 
 const ocrQueue = new Queue('ocrQueue', { connection });
 
